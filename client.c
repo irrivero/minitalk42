42942f9bf6ad85c16f638627784c790971de0048
irrivero <116060502+irrivero@users.noreply.github.com>
2023-08-15 17:43:42 +0200

    Update client.c

--- client.c
+++ client.c
@@ -6,58 +6,85 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:23:41 by irivero-          #+#    #+#             */
-/*   Updated: 2023/08/15 11:00:18 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/08/15 17:41:41 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_binary(char *binary, int bit)
+{
+	int	i;
+
+	i = 0;
+	while (binary[i])
+	{
+		if (binary[i] == '0')
+			kill(bit, SIGUSR2);
+		else if (binary[i] == '1')
+			kill(bit, SIGUSR1);
+		usleep(5000);
+		i++;
+	}
+}
+
+void	send_char(int pid)
+{
+	int		i;
+	int		x;
+	char	*binary;
+	char	*pid1;
+
+	i = 0;
+	pid1 = ft_itoa_pid(getpid());
+	while (pid1[i])
+	{
+		binary = ft_to_binary(pid1[i]);
+		x = 0;
+		while (binary[x])
+		{
+			if (binary[x] == '0')
+				kill(pid, SIGUSR2);
+			else if (binary[x] == '1')
+				kill(pid, SIGUSR1);
+			usleep(1000);
+			x++;
+		}
+		free(binary);
+		i++;
+	}
+	free(pid1);
+}
+
+void	send_string(int signal)
+{
+	if (signal == SIGUSR1)
+	{
+		write(1, "Message received by server\n", 27);
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	pid_t		pid;
-	const char	*str;
-	
-	if (argc != 3)
-	{
-		ft_printf("Usage: %s [Server PID] [Message]\n", argv[0]);
-		return (1);
-	}
-	pid = ft_atoi(argv[1]);
-	str = argv[2];
-	send_string(pid, str);
-	void	*arg;
-}
+	int		i;
+	int		len;
+	char	*bin;
 
-void	send_bit(pid_t pid, int bit)
-{
-	if (bit)
-		kill(pid, SIGUSR2);
+	i = 0;
+	signal(SIGUSR1, send_string);
+	if (argc == 3)
+	{
+		send_char(ft_atoi(argv[1]));
+		len = ft_strlen(argv[2]);
+		while (len >= i)
+		{
+			bin = ft_to_binary(argv[2][i]);
+			send_binary(bin, ft_atoi(argv[1]));
+			free(bin);
+			i++;
+		}
+	}
 	else
-		kill(pid, SIGUSR1);
-	usleep(100);
-}
-
-void	send_char(pid_t pid, char c)
-{
-	int	i;
-
-	i = 0;
-	while (i < 8)
-	{
-		send_bit(pid, (c >> i) & 1);
-		i++;
-	}
-}
-
-void	send_string(pid_t pid, const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		send_char(pid, str[i]);
-		i++;
-	}
-	send_char(pid, '\0');
+		write(1, "Error\n", 6);
+	return (0);
 }

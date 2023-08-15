03b6f5d54f98a1dcfe4e3b2d75951e2e6d07118e
irrivero <116060502+irrivero@users.noreply.github.com>
2023-08-15 17:44:13 +0200

    Update server.c

--- server.c
+++ server.c
@@ -6,52 +6,66 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:23:51 by irivero-          #+#    #+#             */
-/*   Updated: 2023/08/15 11:01:08 by irivero-         ###   ########.fr       */
+/*   Updated: 2023/08/15 17:35:09 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char	g_received_message[4096];
-int		g_received_bit = 0;
+static int	g_int = 0;
+
+void	receive_pid(int n, int l)
+{
+	static int	res = 0;
+
+	if (n >= 0)
+	{
+		res *= 10;
+		res += (n - '0');
+	}
+	if (l)
+	{
+		kill (res, SIGUSR1);
+		g_int = 1;
+		res = 0;
+	}
+}
+
+void	signal_handler(int signum)
+{
+	static int	i = 0;
+	static int	str = 0;
+
+	if (signum == SIGUSR1)
+		str = str | 1;
+	if (++i == 8)
+	{
+		if (str == 27)
+			receive_pid(-1, 1);
+		else if (g_int)
+			write(1, &str, 1);
+		else if (!g_int)
+			receive_pid(str, 0);
+		if (!str)
+		{
+			g_int = 0;
+			write(1, "\n", 1);
+		}
+		str = 0;
+		i = 0;
+	}
+	else
+		str <<= 1;
+}
 
 int	main(void)
 {
-	pid_t	pid;
-
-	pid = getpid();
-	ft_printf("Server PID: %d\n", pid);
-	setup_signals();
+	write(1, "PID: ", 5);
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	while (1)
-	{
-		pause();
-	}
+		pause ();
 	return (0);
-}
-
-void	signal_handler(int signal)
-{
-	if (signal == SIGUSR1)
-		g_received_message[g_received_bit / 8] &= ~(1 << (7 - (g_received_bit % 8)));
-	else if (signal == SIGUSR2)
-		g_received_message[g_received_bit / 8] |= (1 << (7 - (g_received_bit % 8)));
-	g_received_bit++;
-	if (g_received_bit % 8 == 0 && g_received_message[g_received_bit / 8] == '\0')
-	{
-		ft_printf("Received message: %s\n", g_received_message);
-		kill(getpid(), SIGUSR1);
-		g_received_bit = 0;
-		ft_memset(g_received_message, 0, 4096);
-	}
-}
-
-void	setup_signals(void)
-{
-	struct sigaction sa;
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_handler = signal_handler;
-	sigemptyset(&sa.sa_mask);
-
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
 }
